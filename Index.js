6688fca5c84be248ca4aeb3ec7ec0ef70cfec352
galxialdropout <imogen.c.harrison@gmail.com>
2023-08-30 16:48:27 +0100

    Working prompt and computer input.

--- Index.js
+++ Index.js
@@ -1,21 +1,39 @@
-function getComputerChoice() {
-var randomNumber = Math.floor(Math.random()*3);
-    switch (randomNumber) {
-        case 0:
-        randomNumber = 'Rock';
-        break;
-
-        case 1:
-        randomNumber = 'Paper';
-        break;
-
-        case 2:
-        randomNumber = 'Scissors';
-        break;
+function promptResult() {
+    let promptResult = prompt('Rock, Paper, or Scissors?');
+    promptResult = promptResult.toLowerCase();
+    
+    if (promptResult === 'rock' || promptResult === 'paper' || promptResult === 'scissors') {
+        alert(promptResult);
     }
+    else { 
+        alert('Invalid input!');
+        prompt('Please try again');
+     }
 
-    return randomNumber;
+    return promptResult;
 }
 
-alert(getComputerChoice());
+promptResult();
+
+function getComputerChoice() {
+    var randomNumber = Math.floor(Math.random()*3);
+        switch (randomNumber) {
+            case 0:
+            randomNumber = 'Rock';
+            break;
+    
+            case 1:
+            randomNumber = 'Paper';
+            break;
+    
+            case 2:
+            randomNumber = 'Scissors';
+            break;
+        }
+    
+        return randomNumber;
+    }
+
+    console.log(getComputerChoice());
+
 
