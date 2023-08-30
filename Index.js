c4eab9bce70c90f69f9d63fe0aab0f5b795fe091
galxialdropout <imogen.c.harrison@gmail.com>
2023-08-30 16:16:14 +0100

    Built a working computer choice function.

--- Index.js
+++ Index.js
@@ -1,3 +1,21 @@
 function getComputerChoice() {
-    
+var randomNumber = Math.floor(Math.random()*3);
+    switch (randomNumber) {
+        case 0:
+        randomNumber = 'Rock';
+        break;
+
+        case 1:
+        randomNumber = 'Paper';
+        break;
+
+        case 2:
+        randomNumber = 'Scissors';
+        break;
+    }
+
+    return randomNumber;
 }
+
+alert(getComputerChoice());
+
