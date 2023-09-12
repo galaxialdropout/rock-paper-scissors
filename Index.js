ecb4a2c8a4b8c26b9cc9d9a76c3f41d3730309e5
galxialdropout <imogen.c.harrison@gmail.com>
2023-09-12 15:51:41 +0100

    Completed playRound function.

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ function promptResult() {
     promptResult = promptResult.toLowerCase();
     
     if (promptResult === 'rock' || promptResult === 'paper' || promptResult === 'scissors') {
-        alert(promptResult);
+        alert('Thank you!');
     }
     else { 
         alert('Invalid input!');
@@ -12,8 +12,6 @@ function promptResult() {
 
     return promptResult;
 }
-
-promptResult();
 
 function getComputerChoice() {
     var randomNumber = Math.floor(Math.random()*3);
@@ -34,6 +32,59 @@ function getComputerChoice() {
         return randomNumber;
     }
 
-    console.log(getComputerChoice());
+ 
+
+function playRound(playerSelection, computerSelection) {
+alert('You have selected' + ' ' + playerSelection);
+alert('The Computer has selected' + ' ' + computerSelection);
+computerSelection = computerSelection.toLowerCase();
+
+    var computerScore = 0;
+    var userScore = 0;
 
 
+     if (playerSelection === 'rock' && computerSelection === 'rock' || playerSelection === 'paper' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'scissors')  {
+         alert('It is a tie!');
+         
+     } else if (playerSelection === 'rock' && computerSelection === 'paper')  {
+        alert('Computer gets a point! Paper beats rock');
+        computerScore++
+        
+    }  else if (playerSelection === 'rock' && computerSelection === 'scissors')  {
+        alert('Player gets a point! Rock beats scissors');
+        userScore++
+       
+
+    } else if (playerSelection === 'paper' && computerSelection === 'rock')  {
+        alert('Player gets a point! Paper beats rock');
+        userScore++
+       
+
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors')  {
+        alert('Computer gets a point! Scissors beats paper');
+        computerScore++
+
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock')  {
+        alert('Computer gets a point! Scissors beats rock');
+        computerScore++
+       
+
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {  
+        alert('Player wins! Scissors beats paper');
+        userScore++
+        
+    }
+
+    if (computerScore > userScore) {
+        alert('The computer has won this round!');
+    } else if (computerScore < userScore){
+        alert('You win this round!');
+    } else if (computerScore === userScore){
+        alert('This round is a tie!');
+    }
+}
+
+const playerSelection = promptResult();
+const computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rock-paper-scissors
 Odin's first JavaScript project!
+
+
+
+if (computerScore > userScore) {
+        alert('The computer has won this round!');
+    } else if (computerScore < userScore){
+        alert('You win this round!');
+    } else if (computerScore === userScore){
+        alert('This round is a tie!');
+    }
+
+Upgrade function so site plays three rounds and tallies the scores! 
